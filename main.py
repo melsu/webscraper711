3d73f3f45df9dcd97862b1d335d2d393072311c9
Melissa Su <melsu2015@gmail.com>
2022-06-11 01:16:24 +0800

    filter services, initial excel

--- main.py
+++ main.py
@@ -3,22 +3,21 @@ import selenium
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 from bs4 import BeautifulSoup
 import requests
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.action_chains import ActionChains
-# TODO ADD HEADLESS STATE
+import xlsxwriter
+
 #Install Driver
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 stores_url = "https://emap.pcsc.com.tw/emap.aspx#"
 driver.get(stores_url)
 
+# TODO click into all counties and cities
+# for scaling, have list of maplinks and towns
+# click buttons for next pages
 keelung_button = driver.find_element(by=By.XPATH, value='//*[@id="maplink_keelung"]')
 keelung_button.click()
 time.sleep(2)
@@ -27,61 +26,96 @@ zhongzheng_button.click()
 time.sleep(2)  # sleep for two second in the beginning of page and then scroll to the bottom of the page
 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 time.sleep(4) #sleep to get all the items loaded
-
 #on zhongzheng page
 page_source = driver.page_source
 soup = BeautifulSoup(page_source, 'lxml')
-# print('soup:')
-# print(soup)
-# search_resultsXPath = '//*[@id="seardh_bar_list"]'
 
+# Helper functions for webscraping
+
+storeInfoList = ['county', 'district', 'name', 'address']
+# all 35 services available
+servicesList = ['停車場', '廁所', 'ATM', '座位區', 'ibon WiFi', 'OPEN! STORE',
+    '千禧血壓站', '外送咖啡服務', '霜淇淋', '台塑有機蔬菜', '美妝', 'Mister Donut甜甜圈',
+    'ibon', '現萃茶', '現蒸地瓜', '統一生機', 'OPEN!兒童閱覽室', 'K·Seren', '21TOGO',
+    '烘培坊', '!+? CAFE RESERVE', '博客來', '糖果屋', '日本7-ELEVEN限定',
+    '國際精品專櫃', '精品咖啡', 'CITY CAFE氮氣飲品', '天素地蔬', '嚴選素材冷凍鮮物',
+    '光合帕尼尼', '冷凍交貨便', '酒BAR', '寵物生活專區', '酷聖石', '甜點專櫃']
+
+# @arg - table data of one store
+# returns [store name, address]
+def getStoreInfo(td):
+    storeInfo = []
+    tableData = td.find_all('tr')
+    storeNameKey = '門市店名：'
+    storeAddressKey = '地址：'
+    for tr in tableData:
+        data = tr.td
+        if data:
+            info = data.text
+            if storeNameKey in info:
+                storeName = info.split(storeNameKey)[-1]
+                storeInfo.append(storeName)
+            elif storeAddressKey in info:
+                storeAddress = info.split(storeAddressKey)[-1].split('電話：')[0]
+                storeInfo.append(storeAddress)
+    # print(storeInfo)
+    return storeInfo
+
+# @arg - table of services for a single store
+# returns list of services offered at store
+def getServices(servicesTable):
+    services = []
+    # access icons
+    table = servicesTable.tbody.find_all("td", {"class": "icon_sps_li"})
+    for td in table:
+        icon_img = td.img
+        if icon_img:
+            # print(icon_img)
+            serviceName = icon_img.get('title')
+            services.append(serviceName)
+    # print(services)
+    return services
+
+def initExcelSheet():
+    workbook = xlsxwriter.Workbook('storeData711.xlsx')
+    worksheet = workbook.add_worksheet()
+    row = 0
+    col = 0
+    for label in (storeInfoList):
+        worksheet.write(row, col, label)
+        col += 1
+    for service in (servicesList):
+        worksheet.write(row, col, service)
+        col += 1
+    workbook.close()
+
+# @arg - storeInfo [name, address]
+def inputStoreInfoExcel(storeInfo):
+    return
+
+# @arg - services [names of services offered]
+def inputServicesExcel(services):
+    return
+
+initExcelSheet()
+# list of stores on webPage for a city/district
 storesList = soup.find(id='seardh_bar_list', title='storeslist')
-table = storesList.tbody
+table = storesList.tbody #can clean this up
 
-# tr is a table row, represents one store entry
-for tr in table:
-    for td in tr:
-        storeInfo = td.find_all('tr')
-        # text = storeInfo.td.text
-        for tr in storeInfo:
-            # storeAddress = tr.find('地址')
-            if tr.td:
-                print(tr.td.text)
-                print('\n')
-    # addresses = tr.find('地址')
-    # print(addresses)
-    # if table row has data we are looking for
-    # tableData = tr.td
-    # print(tr)
-    # print('\n')
+for store in table:
+    for td in store: # column of info in a row
+        storeInfo = getStoreInfo(td)
+        if storeInfo:
+            inputStoreInfoExcel(storeInfo)
+        servicesTable = td.find("table", {"class": "icon_sps_li"})
+        if servicesTable:
+            services = getServices(servicesTable)
+            if services:
+                inputServicesExcel(services)
+        # print(services)
 
-    # first_td = tr.find('td')
-    # text = first_td.renderContents()
-    # print(text)
-    # print('\n')
-
-# TODO: for some reason full html not being read so can't scrape, close!!!
-# storeName = stores.find('td', align_="center").text
-# storeElements = soup.find_all(string='門市店名：')
-# print(storeElements)
-# for store in storeElements:
-# # a for append, add to file 'w' will overwrite existing info
-#     with open(f'addresses.txt', 'a') as f:
-#         storeName = store.find('td', align_='center').text
-#         print(storeName)
-#         f.write(f"name: {storeName} \n")
-# driver.implicitly_wait(10)
-# ActionChains(driver).move_to_element(button).click(button).perform()
-# while True:
-#     # do whatever you want
-#     try:
-#         driver.find_element_by_xpath('//*[@id="maplink_keelung"]').click()
-#     except NoSuchElementException:
-#         break
-# btn = driver.find_element("maplink_keelung")
-# print(btn)
-# btn.click()
-# print(driver.get(stores_url))
+# put in nice spreadsheet
+# scale up, click through pages and counties (next button)
 
 # returns list of County buttons
 def getCountyButtons():
@@ -102,25 +136,3 @@ def find_stores():
             county = store.text
             print(county)
             f.write(f"name: {county} \n")
-        # if '地址' in store:
-        #     store_name = store.find()
-        #     store_address = store.text
-        #     # more_info = store.header.h2.a['href']
-        #     with open(f'addresses.txt', 'w') as f:
-        #         f.write(f"Store Name: {store_name.strip()}") #strip removes spaces/indents
-        #         f.write(f"Address: {store_address.strip()}")
-        # county.text.replace(' ','')
-    # print(county_name)
-    # open file and read
-    # with open('home.html', 'r') as html_file:
-    #     content = html_file.read() #replace w/ 711 website
-    #     soup = BeautifulSoup(content, 'lxml')
-    #     # use web inspect to find tag
-    #     # addresses = soup.find_all('td', '地址')
-    #     course_tags = soup.find_all('div', class_='card')
-    #     for course in course_tags:
-    #         course_name = course.h5.text
-    #         course_price = course.a.text
-
-# if __name__ == '__main__':
-#     find_stores()
