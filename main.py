6ea51111f7c787339aa1b57d037829e174df4d53
Melissa Su <melsu2015@gmail.com>
2022-06-11 17:31:24 +0800

    scrapes entire district, clicks on next page

--- main.py
+++ main.py
@@ -4,33 +4,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 import time
 
 from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 
-#Install Driver
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-stores_url = "https://emap.pcsc.com.tw/emap.aspx#"
-driver.get(stores_url)
+def initDriver():
+    global driver
+    #Install Driver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    stores_url = "https://emap.pcsc.com.tw/emap.aspx#"
+    driver.get(stores_url)
 
-# TODO click into all counties and cities
-# for scaling, have list of maplinks and towns
-# click buttons for next pages
-keelung_button = driver.find_element(by=By.XPATH, value='//*[@id="maplink_keelung"]')
-keelung_button.click()
-time.sleep(2)
-zhongzheng_button = driver.find_element(by=By.XPATH, value='//*[@id="town_中正區"]')
-zhongzheng_button.click()
-time.sleep(2)  # sleep for two second in the beginning of page and then scroll to the bottom of the page
-driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-time.sleep(4) #sleep to get all the items loaded
-#on zhongzheng page
-page_source = driver.page_source
-soup = BeautifulSoup(page_source, 'lxml')
-
-# Helper functions for webscraping
 def getAllServicesDict():
     global servicesDict
     servicesDict = dict()
@@ -47,8 +35,9 @@ def getAllServicesDict():
 def initExcelSheet():
     global workbook
     global worksheet
-    # global variable to keep track of next excel row
+    # keep track of next excel row
     global row
+    getAllServicesDict()
     storeInfoList = ['county', 'district', 'name', 'address']
     workbook = xlsxwriter.Workbook('storeData711.xlsx')
     worksheet = workbook.add_worksheet()
@@ -61,7 +50,6 @@ def initExcelSheet():
         worksheet.write(row, col, service)
         col += 1
     row = 1
-    # workbook.close()
 
 # @arg - storeInfo [name, address]
 def inputStoreInfoExcel(storeInfo):
@@ -113,44 +101,57 @@ def getServices(servicesTable):
     # print(services)
     return services
 
-getAllServicesDict()
-initExcelSheet()
+# scrapes data from a single webpage
+def scrapeStores(soup):
+    global row
+    # list of stores on webPage for a city/district
+    storesList = soup.find(id='seardh_bar_list', title='storeslist')
+    table = storesList.tbody #can clean this up
+    for store in table:
+        for td in store: # column of info in a row
+            storeInfo = getStoreInfo(td)
+            if storeInfo:
+                inputStoreInfoExcel(storeInfo)
+            servicesTable = td.find("table", {"class": "icon_sps_li"})
+            if servicesTable:
+                services = getServices(servicesTable)
+                inputServicesExcel(services)
+                row += 1 #increment excel row
 
-# list of stores on webPage for a city/district
-storesList = soup.find(id='seardh_bar_list', title='storeslist')
-table = storesList.tbody #can clean this up
+def main():
+    global soup
+    initDriver()
+    # TODO click into all counties and cities
+    # for scaling, have list of maplinks and towns
+    # click buttons for next pages
+    keelung_button = driver.find_element(by=By.XPATH, value='//*[@id="maplink_keelung"]')
+    keelung_button.click()
 
-for store in table:
-    for td in store: # column of info in a row
-        storeInfo = getStoreInfo(td)
-        if storeInfo:
-            inputStoreInfoExcel(storeInfo)
-        servicesTable = td.find("table", {"class": "icon_sps_li"})
-        if servicesTable:
-            services = getServices(servicesTable)
-            inputServicesExcel(services)
-            row += 1 #increment excel row
-        # print(services)
-workbook.close()
-# put in nice spreadsheet
-# scale up, click through pages and counties (next button)
+    zhongzheng_button = driver.find_element(by=By.XPATH, value='//*[@id="town_中正區"]')
+    zhongzheng_button.click()
+    time.sleep(2)  # sleep for two second in the beginning of page and then scroll to the bottom of the page
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(4) #sleep to get all the items loaded
+    #on zhongzheng page
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'lxml')
+    initExcelSheet()
+    scrapeStores(soup)
+    try:
+        nextButton = driver.find_element(by=By.XPATH, value='//*[@id="Next"]')
+    except NoSuchElementException:
+        nextButton = False
+    while nextButton:
+        nextButton.click()
+        time.sleep(2)
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, 'lxml')
+        scrapeStores(soup)
+        try:
+            nextButton = driver.find_element(by=By.XPATH, value='//*[@id="Next"]')
+        except NoSuchElementException:
+            nextButton = False
+    workbook.close()
 
-# returns list of County buttons
-# def getCountyButtons():
-#     buttonElements = []
-#     driver.find_element_by_id("a")
-#     return buttonElements
-#
-# def find_stores():
-#     html_text = requests.get(stores_url).text
-#     # simulate click for keelung
-#     # <a href="#" class="n_1" id="maplink_keelung">基隆市</a>
-#     soup = BeautifulSoup(html_text, 'lxml')
-#     # find(htmlTag, )
-#     stores = soup.find_all('a', class_="n_1", string="地址")
-#     for store in stores:
-#     # a for append, add to file 'w' will overwrite existing info
-#         with open(f'addresses.txt', 'a') as f:
-#             county = store.text
-#             print(county)
-#             f.write(f"name: {county} \n")
+if __name__ == "__main__":
+    main()
