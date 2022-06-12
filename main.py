cfcb68bb8035b073bf134eb8e06af4768f8c2e7d
Melissa Su <melsu2015@gmail.com>
2022-06-12 21:28:21 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -5,16 +5,13 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-
 import time
-
 from bs4 import BeautifulSoup
 import requests
 import xlsxwriter
 
 def initDriver():
     global driver
-    #Install Driver
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     stores_url = "https://emap.pcsc.com.tw/emap.aspx#"
     driver.get(stores_url)
@@ -38,7 +35,7 @@ def initExcelSheet():
     # keep track of next excel row
     global row
     getAllServicesDict()
-    storeInfoList = ['county', 'district', 'name', 'address']
+    storeInfoList = ['county', 'town', 'name', 'address']
     workbook = xlsxwriter.Workbook('storeData711.xlsx')
     worksheet = workbook.add_worksheet()
     row = 0
@@ -52,9 +49,11 @@ def initExcelSheet():
     row = 1
 
 # @arg - storeInfo [name, address]
-def inputStoreInfoExcel(storeInfo):
+def inputStoreInfoExcel(county, town, storeInfo):
     print(storeInfo)
     print(row)
+    worksheet.write(row, 0, county)
+    worksheet.write(row, 1, town)
     worksheet.write(row, 2, storeInfo[0])
     worksheet.write(row, 3, storeInfo[1])
 
@@ -83,7 +82,6 @@ def getStoreInfo(td):
             elif storeAddressKey in info:
                 storeAddress = info.split(storeAddressKey)[-1].split('電話：')[0]
                 storeInfo.append(storeAddress)
-    # print(storeInfo)
     return storeInfo
 
 # @arg - table of services for a single store
@@ -95,14 +93,12 @@ def getServices(servicesTable):
     for td in table:
         icon_img = td.img
         if icon_img:
-            # print(icon_img)
             serviceName = icon_img.get('title')
             services.append(serviceName)
-    # print(services)
     return services
 
 # scrapes data from a single webpage
-def scrapeStores(soup):
+def scrapeStores(soup, county, town):
     global row
     # list of stores on webPage for a city/district
     storesList = soup.find(id='seardh_bar_list', title='storeslist')
@@ -111,46 +107,67 @@ def scrapeStores(soup):
         for td in store: # column of info in a row
             storeInfo = getStoreInfo(td)
             if storeInfo:
-                inputStoreInfoExcel(storeInfo)
+                inputStoreInfoExcel(county, town, storeInfo)
             servicesTable = td.find("table", {"class": "icon_sps_li"})
             if servicesTable:
                 services = getServices(servicesTable)
                 inputServicesExcel(services)
                 row += 1 #increment excel row
 
+# returns list of names for towns in a county
+def getTownNames(soup):
+    townHTMLElements = soup.find(id="counties_s_li", title="countrieslist")
+    townLinks = townHTMLElements.find_all('a')
+    townIDs = []
+    for c in townLinks:
+        # townIDs.append(c['id'])
+        townIDs.append(c.text.strip())
+    return townIDs
+
 def main():
     global soup
+    waitTime = 2
     initDriver()
     # TODO click into all counties and cities
     # for scaling, have list of maplinks and towns
     # click buttons for next pages
-    keelung_button = driver.find_element(by=By.XPATH, value='//*[@id="maplink_keelung"]')
-    keelung_button.click()
 
-    zhongzheng_button = driver.find_element(by=By.XPATH, value='//*[@id="town_中正區"]')
-    zhongzheng_button.click()
-    time.sleep(2)  # sleep for two second in the beginning of page and then scroll to the bottom of the page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(4) #sleep to get all the items loaded
-    #on zhongzheng page
+    # go back to homepage with map
+    keelung_button = driver.find_element(by=By.XPATH, value='//*[@id="maplink_keelung"]')
+    time.sleep(waitTime)
+    keelung_button.click()
     page_source = driver.page_source
     soup = BeautifulSoup(page_source, 'lxml')
     initExcelSheet()
-    scrapeStores(soup)
-    try:
-        nextButton = driver.find_element(by=By.XPATH, value='//*[@id="Next"]')
-    except NoSuchElementException:
-        nextButton = False
-    while nextButton:
-        nextButton.click()
-        time.sleep(2)
+    townNames = getTownNames(soup)
+    county = 'filler'
+    # in county
+    for town in townNames:
+        townButton = driver.find_element(by=By.LINK_TEXT, value=town)
+        townButton.click()
+        time.sleep(waitTime) #sleep to get all the items loaded
+        #on town page
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'lxml')
-        scrapeStores(soup)
+        scrapeStores(soup, county, town)
         try:
             nextButton = driver.find_element(by=By.XPATH, value='//*[@id="Next"]')
         except NoSuchElementException:
             nextButton = False
+        while nextButton:
+            nextButton.click()
+            time.sleep(waitTime)
+            page_source = driver.page_source
+            soup = BeautifulSoup(page_source, 'lxml')
+            scrapeStores(soup, 'keelung_county_filler', town)
+            try:
+                nextButton = driver.find_element(by=By.XPATH, value='//*[@id="Next"]')
+            except NoSuchElementException:
+                nextButton = False
+        # click back to list of towns page in county
+        countyHomePage = driver.find_element(by=By.XPATH, value='//*[@id="map_all_1"]/a')
+        countyHomePage.click()
+        time.sleep(waitTime) #sleep to get all the items loaded
     workbook.close()
 
 if __name__ == "__main__":
